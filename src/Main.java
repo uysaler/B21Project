d9653f51d91d9eb2390d2f4e12bf5e61a0fde87a
uysaler <aysepernur@gmail.com>
2020-11-10 11:26:39 -0600

    added new

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //B21 is good at IntelliJ
-        //...
+        //...more staff
+        //happy to see
     }
 }
